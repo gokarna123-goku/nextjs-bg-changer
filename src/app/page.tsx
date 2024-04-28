fa22c0a05d96932bea73e76ea200223f8e2ea64c
Gokarna Adhikari <agokarna444@gmail.com>
2024-04-28 10:06:54 +0545

    complete ui part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex-1 flex-col gap-12">
+    <div className="flex-1 flex flex-col gap-16">
       {/* Top Section */}
       <div className="w-full h-auto flex items-center justify-center">
         <TopContainer />
       </div>
       {/* Photo Container */}
-      <div className="w-full h-full flex items-center justify-center">
+      <div className="w-full h-fit flex items-center justify-center">
         <PhotoContainer />
       </div>
     </div>
--- src/pages/photo/index.tsx
+++ src/pages/photo/index.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PhotoContainer: React.FC = () => {
     return (
-        <div className='w-1/4 border-2 border-neutral-200 dark:border-neutral-800 rounded-md p-3'>
-            This is the Photo Container.
+        <div className='w-1/2 border-2 border-neutral-200 dark:border-neutral-800 rounded-2xl p-4'>
+            <Image src="https://source.unsplash.com/random/?india" alt="photo" width={500} height={500} className='w-full aspect-auto rounded-xl object-cover object-center' />
         </div>
     )
 }
